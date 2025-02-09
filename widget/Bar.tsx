efa04d38fc5fad96203031b3f1026fc3b1dc7b90
joseporcar <joseaporcar@gmail.com>
2025-02-09 15:34:55 -0400

    basic structure set

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -2,19 +2,12 @@ import { App, Astal, Gtk, Gdk } from "astal/gtk4"
 import { Variable, bind } from "astal"
 import Hyprland from "gi://AstalHyprland"
 
-const time = Variable("").poll(1000, "date")
-const hypr = Hyprland.get_default();
+function Workspaces() {
+    const hypr = Hyprland.get_default()
+}
 export default function Bar(gdkmonitor: Gdk.Monitor) {
     const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
 
-    const count = Variable(1);
-    function increase() {
-        count.set(count.get() * 2)
-    }
-
-    function decrease() {
-        count.set(count.get() - 1)
-    }
     return <window
         visible
         cssClasses={["Bar"]}
@@ -22,13 +15,20 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
         exclusivity={Astal.Exclusivity.EXCLUSIVE}
         anchor={TOP | LEFT | RIGHT}
         application={App}>
+        
         <centerbox cssName="centerbox">
+            <box>
+                <label label="hellowrold" /> 
+            </box>
 
-            
+            <box>
 
-            <label label="hellowrold" /> 
-            <button label={bind(count).as(x=>x.toString())} onClicked={increase}/>
+            </box>
 
+            <box> 
+                <label label="arst" /> 
+                <label label="p" /> 
+            </box>
         </centerbox>
     </window>
 }
