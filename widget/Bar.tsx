315510a03f85deb7cb9e61196dd7780dcd80d639
joseporcar <joseaporcar@gmail.com>
2025-02-09 16:34:18 -0400

    working workspaces

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -1,9 +1,25 @@
 import { App, Astal, Gtk, Gdk } from "astal/gtk4"
 import { Variable, bind } from "astal"
 import Hyprland from "gi://AstalHyprland"
+import AstalHyprland from "gi://AstalHyprland"
 
 function Workspaces() {
     const hypr = Hyprland.get_default()
+
+    function workspaceParser(workspaces: AstalHyprland.Workspace[]) {
+        return workspaces
+            .filter(ws => ws.id > 0)
+            .sort((a,b) => a.id - b.id)
+            .map(ws => <button
+                onClicked={() => ws.focus()}
+                label={bind(hypr, "focusedWorkspace").as(a => a === ws ? "|" : "–")}
+            />)
+    }
+    return <box> 
+        {
+        bind(hypr, "workspaces").as(workspaceParser)
+        }
+    </box>
 }
 export default function Bar(gdkmonitor: Gdk.Monitor) {
     const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
@@ -18,11 +34,12 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
         
         <centerbox cssName="centerbox">
             <box>
+                <Workspaces />
                 <label label="hellowrold" /> 
             </box>
 
             <box>
-
+                
             </box>
 
             <box> 
