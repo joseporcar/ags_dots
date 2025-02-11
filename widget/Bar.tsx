db7c868d2b5e3a32de7d9f60890152c64c770b37
joseporcar <joseaporcar@gmail.com>
2025-02-11 08:49:00 -0400

    Fixed workspaces and changed font to IBM Plex Mono

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -1,31 +1,28 @@
 import { App, Astal, Gtk, Gdk } from "astal/gtk4"
 import { Variable, bind, GLib } from "astal"
 import Hyprland from "gi://AstalHyprland"
-import AstalHyprland from "gi://AstalHyprland"
 import Battery from "gi://AstalBattery"
 
 function Workspaces() {
     const hypr = Hyprland.get_default()
-
-    function workspaceParser(workspaces: AstalHyprland.Workspace[]) {
-        return workspaces
+    const fw = bind(hypr, "focusedWorkspace")
+    return <box cssClasses={["workspaces"]}> 
+        {
+        bind(hypr, "workspaces").as(workspaces => workspaces
             .filter(ws => ws.id > 0)
             .sort((a,b) => a.id - b.id)
             .map(ws => <button
-                css_classes={["workspace"]}
+                css_classes={fw.as(fw => fw === ws ? ["focused"] : ["unfocused"])}
                 onClicked={() => ws.focus()}
-                label={bind(hypr, "focusedWorkspace").as(a => a === ws ? "|" : "–")}
+                label={fw.as(fw => fw === ws ? "|" : "—")}
             />)
-    }
-    return <box> 
-        {
-        bind(hypr, "workspaces").as(workspaceParser)
+        )
         }
     </box>
 }
 
 function DateTime() { 
-    const datetime = Variable<string>("").poll(5000, () => GLib.DateTime.new_now_local().format("%Y-%m-%d %H:%M")!)
+    const datetime = Variable<string>("").poll(5000, () => GLib.DateTime.new_now_local().format("%m-%d –– %H:%M")!)
     return <label 
         onDestroy={() => datetime.drop()}
         label={datetime()}
