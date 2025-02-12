92d2f024446be70950efa80aad8fde44516dc164
joseporcar <joseaporcar@gmail.com>
2025-02-12 12:24:34 -0400

    reformatting

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -6,17 +6,21 @@ import Notifd from "gi://AstalNotifd"
 
 function Bat() {
     const battery = Battery.get_default()
-    return <label
+    return <box>
+        <label
         label={bind(battery, "percentage").as(n => Math.round(n * 100).toString()+"%")}
     />
+    </box>
 }
 
 function DateTime() { 
     const datetime = Variable<string>("").poll(5000, () => GLib.DateTime.new_now_local().format("%m-%d  —  %H:%M")!)
-    return <label 
+    return <box>
+        <label 
         onDestroy={() => datetime.drop()}
         label={datetime()}
     />
+    </box>
 }
 
 function Notifications() {
@@ -36,7 +40,6 @@ function Workspaces() {
     return <box cssClasses={["workspaces"]}> 
         {
         [1, 2, 3, 4, 5]
-            .sort((a, b) =>  a - b)
             .map(ws => <button
                 css_classes={fw.as(fw => fw.id === ws ? ["focused"] : ["unfocused"])}
                 onClicked={() => hypr.dispatch("focusworkspaceoncurrentmonitor", ws.toString())}
