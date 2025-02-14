38860d4c9804b0ae9a3e632f86d5d8506c1d721b
joseporcar <joseaporcar@gmail.com>
2025-02-14 06:16:59 -0400

    volume bar now works? only changes volume on release but oh well. Without the dragged I am not very sure how to work it

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -8,22 +8,26 @@ import Wp from "gi://AstalWp"
 function Audio() {
     const speaker = Wp.get_default()?.audio.defaultSpeaker!;
     var mute = bind(speaker, "mute");
-    
+
     return <box cssClasses={["volume"]}>
         <button
             onClicked={() => speaker.mute = !speaker.mute}>
             {mute.as(m => m ? "🤫" : "🗣️")}
         </button>
-        <slider
+        {<slider
             cssClasses={mute.as(m => m ? ["muted"] : ["unmuted"])}
             widthRequest={200}
             value={bind(speaker, "volume").as(v => v * 100)}   
-            //onChangeValue={()}
+            onScroll={(v,_, y) => speaker.volume = Math.max(speaker.volume + y / 100, 0)}
+            //onDragged={({value}) => speaker.volume = value / 100}
+            //onChangeValue={val => speaker.volume = val.value / 100}
+            onButtonReleased={(slider, event) => speaker.volume = slider.value / 100}            
+            //onMotion={(v,x, _) => speaker.volume = x / 100}
             digits={0}
             drawValue={true}
             adjustment={Gtk.Adjustment.new(0, 0, 150, 1, 0, 0)}            
-            valuePos={Gtk.PositionType.RIGHT}>
-        </slider>
+            valuePos={Gtk.PositionType.RIGHT}
+        /> }
     </box>
 }
 function Bat() {
